2bd55836918b77c555ed048c68b6d1fe45f35626
Frenzy265 <klaus_franziska@gmx.de>
2020-10-27 10:16:44 +0100

    Lifting srtate up

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,10 +2,9 @@ import "./square.css";
 import React, { useState } from "react";
 
 export default function Square(props) {
-  const [value, setValue] = useState(null);
   return (
-    <button className="square" onClick={() => setValue("X")}>
-      {value}
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
     </button>
   );
 }
