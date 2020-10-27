995ed932830f952310d73044cf01cc99a2a117b8
Frenzy265 <klaus_franziska@gmx.de>
2020-10-27 10:49:42 +0100

    Declare a winner

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,7 +6,18 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   function handleClick(index) {
+    if (calculateWinner(squares) || squares[index]) {
+      return;
+    }
     const newSquares = squares.slice();
     newSquares[index] = xIsNext ? "X" : "O";
     setSquares(newSquares);
@@ -17,7 +28,7 @@ export default function Board() {
     return <Square value={squares[index]} onClick={() => handleClick(index)} />;
   }
 
-  const status = "Next player: " + (xIsNext ? "X" : "O");
+  // const status = "Next player: " + (xIsNext ? "X" : "O");
 
   return (
     <div>
@@ -39,4 +50,24 @@ export default function Board() {
       </div>
     </div>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
